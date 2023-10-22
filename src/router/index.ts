14a81b6cabf6a8666445f2657d233923dc479f5a
wemsx <3551697397@qq.com>
2023-10-22 10:41:54 +0800

    normal: 删除废弃依赖*2。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-const _isMobile = () => {
+/*const _isMobile = () => {
   const flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
   return flag;
-}
+}*/
 const pcr = [
   {
     path: '/',
@@ -18,13 +18,13 @@ const pcr = [
     component: () => import('../views/MainView.vue')
   }
 ]
-const mbr = [
+/*const mbr = [
   {
     path: '/',
     name: 'home',
     component: () => import('../MobileApp.vue')
   }
-]
+]*/
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
