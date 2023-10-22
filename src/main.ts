b02a2a461451b7fc69db8998ce18585075f7935b
wemsx <3551697397@qq.com>
2023-10-22 10:39:22 +0800

    normal: 删除了废弃依赖。

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import "./assets/main.css";
 import 'amfe-flexible'
 
 import App from "./App.vue";
-import App_m from "./MobileApp.vue";
 import router from "./router";
-const _isMobile = () => {
+
+/*const _isMobile = () => {
   const flag = navigator.userAgent.match(
     /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
   );
   return flag;
-};
+};*/
+
 /*const app = createApp(_isMobile() ? App_m : App);*/
+
 const app = createApp(App)
 import pinia from "./stores/index";
 
